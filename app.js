9fcc9a59cb8fe0bb611d73f255d09c43cdf0baea
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-03-22 18:21:07 +0100

    task completed

---
+++ app.js
@@ -0,0 +1,12 @@
+const name = "Joshua Adesanya";
+
+const courses = ["Html", "CSS", "Javascript", "php", "nodejs", "golang", "C#"];
+
+console.log(name);
+console.log(courses);
+
+for (let i = 1; i <= 200; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
