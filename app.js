e95ca48a4b3982adb89bf1572b263b983c043f09
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-03-22 18:23:56 +0100

    new changes

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+//name
 const name = "Joshua Adesanya";
 
+// Courses
 const courses = ["Html", "CSS", "Javascript", "php", "nodejs", "golang", "C#"];
 
+//display name
 console.log(name);
+
+//display course
 console.log(courses);
 
-for (let i = 1; i <= 200; i++) {
-  if (i % 2 !== 0) {
-    console.log(i);
+//display all odd numbers from 0 to 200 inclusive
+for (let odd = 1; odd <= 200; odd++) {
+  if (odd % 2 !== 0) {
+    console.log(odd);
   }
 }
